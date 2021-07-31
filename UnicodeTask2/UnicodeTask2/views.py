91a3f9833b17e54874d41af8f3e582c73294176f
Pratham <prthmbhr@gmail.com>
2021-08-01 01:51:57 +0530

    Unicode Task2

---
+++ UnicodeTask2/UnicodeTask2/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+import requests
+import json
+
+def pokemon(request):
+    x=requests.get('https://pokeapi.co/api/v2/type')
+    poketype=x.json()    #Also can use poketype=json.loads(x.text)
+    output=''
+    for i in poketype["results"]:
+        output=output + '\n' +  i['name'] 
+    return HttpResponse('<h1>Pokemon Types</h1>'+ output)
