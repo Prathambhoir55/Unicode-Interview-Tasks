ff0eebe6dd7075d9ce47e1bd49182e2db8b0271d
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2021-08-08 01:15:21 +0530

    task 3

--- UnicodeTask2/UnicodeTask2/settings.py
+++ UnicodeTask2/UnicodeTask2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'UnicodeTask2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- UnicodeTask2/UnicodeTask2/views.py
+++ UnicodeTask2/UnicodeTask2/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse
 import requests
+from django.shortcuts import render
 import json
 
-def pokemon(request):
+def index(request):
+    return render(request, 'index.html')
+
+def task2(request):
     x=requests.get('https://pokeapi.co/api/v2/type')
     poketype=x.json()    #Also can use poketype=json.loads(x.text)
     output=''
     for i in poketype["results"]:
-        output=output + ' ' +  i['name'] 
-    return HttpResponse('<h1>Pokemon Types</h1>'+ output)
+        output=output + '<li>' +  i['name'] +'</li>'
+    return HttpResponse('<h1>Pokemon Types</h1>'+ '<ol>'+ output +'</ol>')
+
+def task3(request):
+    return render(request, 'task3.html')
+
+def pokemon(request):
+    type=request.GET.get('text')
+    x=requests.get('https://pokeapi.co/api/v2/type')
+    poketype=x.json() 
+    output=''   
+    for i in poketype["results"]:
+        if type==i['name']:
+            k=requests.get(i['url'])
+            poketype2=k.json()
+            for j in poketype2["pokemon"]:
+                output=output + '<li>' + j['pokemon']['name'] + '</li>'
+    return HttpResponse('<h1>Pokemon</h1>'+ '<ul>' + output + '</ul>')
